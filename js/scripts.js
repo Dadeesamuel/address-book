6cfccf1db8f367b9b1b3f911f50e9161a4181427
DADEE SAMUEL <dadeesamuel@gmail.com>
2021-11-12 11:16:05 +0100

    speaking voice

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
+//business logic
+const speak= (msg) =>{
+    const sp= new
+    SpeechSynthesisUtterance(msg);
+    [sp.voice]= speechSynthesis.getVoices();
+    speechSynthesis.speak(sp);
+}
+
+//user interface
 $(document).ready(function(){
     $(".data").submit(function(event){
+    
         const firstName = $("#firstname").val();
         const middleName = $("#middlename").val();
         const lastName = $("#lastname").val();
@@ -16,5 +26,20 @@ $(".m").text(fullName)
 $(".e").text(phonenumber)
 $(".t").text(email)
 
+
+$(".speak").click(function(){
+ speak(fullName);
+ speak(phonenumber);
+ speak(email);
+});
+ /*function printElem(){
+     var mywindow = window.open('', 'PRINT');
+     mywindow.document.write('<html><head><title>address-book</title>')
+     mywindow.document.write('</head></html>');
+     mywindow.print();
+     mywindow.close();
+     return true;
+ }
+printElem();*/
     });
 });
